e448708066bb3418525526cdff4c189f5b8f77c5
asd42270 <lyb991113@gmail.com>
2024-08-12 05:50:35 +0900

    임시 주석 처리

--- com.example.msa_exam.order/src/main/java/com/spring_cloud/eureka/client/order/controller/ProductClient.java
+++ com.example.msa_exam.order/src/main/java/com/spring_cloud/eureka/client/order/controller/ProductClient.java
@@ -1,6 +1,6 @@
 package com.spring_cloud.eureka.client.order.controller;
 
-import com.spring_cloud.eureka.client.product.entity.dto.ProductResponseDto;
+//import com.spring_cloud.eureka.client.product.entity.dto.ProductResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,7 +8,7 @@ import java.util.List;
 
 @FeignClient(name = "product-service", url = "http://localhost:19091/products")
 public interface ProductClient {
-    @GetMapping
-    List<ProductResponseDto> getProducts();
+//    @GetMapping
+//    List<ProductResponseDto> getProducts();
 
 }
