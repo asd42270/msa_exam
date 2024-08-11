11440b67d7320491d45c46fc00082b1744381b8e
asd42270 <lyb991113@gmail.com>
2024-08-12 03:22:49 +0900

    FeignClient 어노테이션 추가

--- com.example.msa_exam.order/src/main/java/com/spring_cloud/eureka/client/order/OrderApplication.java
+++ com.example.msa_exam.order/src/main/java/com/spring_cloud/eureka/client/order/OrderApplication.java
@@ -2,8 +2,10 @@ package com.spring_cloud.eureka.client.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@FeignClient
 public class OrderApplication {
 
 	public static void main(String[] args) {
