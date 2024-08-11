44d6a291af62e3697263695e9e5e8e93fb461afa
asd42270 <lyb991113@gmail.com>
2024-08-12 03:39:58 +0900

    Header에 Port 추가

--- com.example.msa_exam.product/src/main/java/com/spring_cloud/eureka/client/product/controller/ProductController.java
+++ com.example.msa_exam.product/src/main/java/com/spring_cloud/eureka/client/product/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.spring_cloud.eureka.client.product.entity.dto.ProductInsertRequestDto
 import com.spring_cloud.eureka.client.product.entity.dto.ProductResponseDto;
 import com.spring_cloud.eureka.client.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +19,26 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Value("${server.port}")
+    private String serverPort;
     @GetMapping()
     public ResponseEntity<List<ProductResponseDto>> getProducts() {
-        return ResponseEntity.ok(productService.getProducts());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Server-port", serverPort);
+        return ResponseEntity.ok()
+                .headers(httpHeaders)
+                .body(productService.getProducts());
     }
 
     @PostMapping()
     public ResponseEntity<Void> addProduct(
             @RequestBody ProductInsertRequestDto requestDto
     ) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Server-port", serverPort);
         productService.addProduct(requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .headers(httpHeaders)
+                .build();
     }
 }
