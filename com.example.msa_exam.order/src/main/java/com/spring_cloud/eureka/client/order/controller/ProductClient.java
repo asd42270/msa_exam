40c53df439e620bffc9fff6bfba37cad6a7541c1
asd42270 <lyb991113@gmail.com>
2024-08-12 05:46:09 +0900

    Feign 클라이언트 파일 추가

---
+++ com.example.msa_exam.order/src/main/java/com/spring_cloud/eureka/client/order/controller/ProductClient.java
@@ -0,0 +1,14 @@
+package com.spring_cloud.eureka.client.order.controller;
+
+import com.spring_cloud.eureka.client.product.entity.dto.ProductResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "product-service", url = "http://localhost:19091/products")
+public interface ProductClient {
+    @GetMapping
+    List<ProductResponseDto> getProducts();
+
+}
