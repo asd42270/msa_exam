bbbb0f8553f69919b216d19d0a194fb2e01d937d
asd42270 <lyb991113@gmail.com>
2024-08-12 03:42:10 +0900

    Header에 Port 추가

--- com.example.msa_exam.order/src/main/java/com/spring_cloud/eureka/client/order/controller/OrderController.java
+++ com.example.msa_exam.order/src/main/java/com/spring_cloud/eureka/client/order/controller/OrderController.java
@@ -5,6 +5,8 @@ import com.spring_cloud.eureka.client.order.entity.dto.OrderResponseDto;
 import com.spring_cloud.eureka.client.order.entity.dto.OrderUpdateRequestDto;
 import com.spring_cloud.eureka.client.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +18,20 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping()
     public ResponseEntity<Void> addOrder(
             @RequestBody OrderInsertRequestDto requestDto
     ) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Server-port", serverPort);
+
         orderService.addOrder(requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .headers(httpHeaders)
+                .build();
     }
 
     @PutMapping("/{orderId}")
@@ -29,16 +39,24 @@ public class OrderController {
             @PathVariable("orderId") Long orderId,
             @RequestBody OrderUpdateRequestDto requestDto
     ) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Server-port", serverPort);
+
         orderService.updateOrder(orderId, requestDto);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(HttpStatus.OK)
+                .headers(httpHeaders)
+                .build();
     }
 
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponseDto> getOrder(
             @PathVariable("orderId") Long orderId
     ) {
-        return ResponseEntity.ok(orderService.getOrder(orderId));
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Server-port", serverPort);
+
+        return ResponseEntity.ok()
+                .headers(httpHeaders)
+                .body(orderService.getOrder(orderId));
     }
-
-
 }
